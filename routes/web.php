59ccc14d05f6aabe1d91044521b181e208a44387
stick231 <veniamin7766z@gmail.com>
2024-12-04 23:08:48 +0300

    create-logic-crud

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\RecordController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-
+Route::get('/', function(){
+    return view('welcome');
 });
+
+Route::resource('record', RecordController::class);
